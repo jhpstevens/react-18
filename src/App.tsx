9247711e09eb4dbfb5e24a4fd8055bd8e67f3345
jhpstevens <49429079+jhpstevens@users.noreply.github.com>
2023-08-15 15:06:54 +0200

    2 files modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
-import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+
+
 
 function App() {
+  //dummy data for testing
+  const expenses = [
+    {id: 1, description: 'aaa', amount: 10, category: 'Utilities'},
+    {id: 2, description: 'bbb', amount: 20, category: 'Utilities'},
+    {id: 3, description: 'ccc', amount: 30, category: 'Utilities'},
+    {id: 4, description: 'ddd', amount: 40, category: 'Utilities'},
+  ]
+
   return (
     <div>
-      <Form></Form>
+      <ExpenseList expenses={ expenses } onDelete={(id) => console.log('Delete', id)}/>
     </div>
   );
 }
