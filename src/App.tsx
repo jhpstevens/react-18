6d40f9f2f192f79f9f5800803f7599a3bcb217a2
jhpstevens <49429079+jhpstevens@users.noreply.github.com>
2023-08-17 10:06:10 +0200

    5 files modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { useState } from "react";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-
-export const categories = ["Groceries", "Utilities", "Entertainment"];
+import { useState } from 'react';
+import ExpenseList from './expense-tracker/components/ExpenseList';
+import ExpenseFilter from './expense-tracker/components/ExpenseFilter';
+import ExpenseForm from './expense-tracker/components/ExpenseForm';
+import categories from './expense-tracker/categories';
 
 function App() {
   //dummy data for testing
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState('');
   const expenses = [
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 20, category: "Utilities" },
-    { id: 3, description: "ccc", amount: 30, category: "Groceries" },
-    { id: 4, description: "ddd", amount: 40, category: "Utilities" },
+    { id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
+    { id: 2, description: 'bbb', amount: 20, category: 'Utilities' },
+    { id: 3, description: 'ccc', amount: 30, category: 'Groceries' },
+    { id: 4, description: 'ddd', amount: 40, category: 'Utilities' },
   ];
 
   const visibleExpenses = selectedCategory
@@ -31,7 +30,7 @@ function App() {
       </div>
       <ExpenseList
         expenses={visibleExpenses}
-        onDelete={(id) => console.log("Delete", id)}
+        onDelete={(id) => console.log('Delete', id)}
       />
     </div>
   );
